66ca2804ec9f76bf17550d431ca5c4e33c9ecba4
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 12:26:23 +0330

    Changed the button type from `submit` to `button` in the navigation links.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import Users from "./Components/Users"
 import Photoes from "./Components/Photoes"
 
 function App() {
-
   return (
     <>
       <h1>useEffect</h1>
-      <NavLink  to="Photoes" className='btn ms-lg-3 fs-small btn-opt-nj border-0'>Photoes</NavLink>
-      <NavLink  to="/" className='btn ms-lg-3 fs-small btn-opt-nj border-0'>Users</NavLink>
-
+      <NavLink to="Photoes" className='btn ms-lg-3 fs-small btn-opt-nj border-0' type="button">Photoes</NavLink>
+      <NavLink to="/" className='btn ms-lg-3 fs-small btn-opt-nj border-0'>Users</NavLink>
       <Routes>
         <Route path="/" element={<Users />} />
-        <Route path="Photoes" element={<Photoes/>} />
+        <Route path="Photoes" element={<Photoes />} />
       </Routes>
-
     </>
   )
 }
