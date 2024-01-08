2fa4df369d89448ed6a448a5c27a92319a5bc507
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 11:43:27 +0330

    Update useEffect dependency in Users component

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,49 +1,66 @@
+//sample: useEffect(()=>{function()}, [dependency])
 import { useEffect, useState } from "react"
 
 const Users = () => {
     const [users, setUsers] = useState([]);
     const [error, setError] = useState(false);
+    // set dependency
+    const [id, setId] = useState("");
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((res) => res.json())
             .then((json) => setUsers(json))
             .catch(() => setError(true));
-    }, []); 
+        console.log('useEffect runned!')
+    }, [id]); //dependency
     // arrow function
- /*     const fetchUsersWithAsyncAndArrow = async () => {
-        try {
-            const res = await fetch('https://jsonplaceholder.typicode.com/users');
-            const json = await res.json();
-            setUsers(json);
-        } catch (error) {
-            console.log(error);
-            setError(true)
-        }
-    }  
-    useEffect(()=>{
-       fetchUsersWithAsyncAndArrow();
-    })
-    */
+    /*   useEffect(() => {
+           const fetchUsersWithAsyncAndArrow = async () => {
+               try {
+                   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+                   const json = await res.json();
+                   setUsers(json);
+               } catch (error) {
+                   console.log(error);
+                   setError(true);
+               }
+           };
+   
+           fetchUsersWithAsyncAndArrow();
+       }, []); 
+       */
     //async / await
-/*     useEffect(() => {
-        async function fetchUsersWithAsync() {
-            const res = await fetch('https://jsonplaceholder.typicode.com/users');
-            const json = await res.json;
-            setUsers(json);
-        }; 
-        fetchUsersWithAsync();
-    }, []); */
+    /*    useEffect(() => {
+            const fetchUsersWithAsync = async () => {
+                try {
+                    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+                    const json = await res.json();
+                    setUsers(json);
+                } catch (error) {
+                    console.log(error);
+                }
+            };
+    
+            fetchUsersWithAsync();
+        }, []); */
+    const searchHandler = async () => {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const json = await res.json();
+        console.log(json)
+    }
     return (
         <div>
-        {!users.length && !error && <p>Loading...</p>}
-        <ul>
-            {users.map(user =>
-                <li key={user.id}>{user.name} {user.username}</li>
-            )}
-        </ul>
-        {error && <p>Something went wrong!</p>}
-    </div>
+            <input type="text" value={id} onChange={e => setId(e.target.value)} placeholder="Enter id" />
+            <button onClick={searchHandler}>Search</button>
+            {!users.length && !error && <p>Loading...</p>}
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>{user.name} {user.username}</li>
+                )}
+            </ul>
+            {error && <p>Something went wrong!</p>}
+        </div>
     )
 }
 
