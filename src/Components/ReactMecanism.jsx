d27707a898a066f101fd48898a1e5710fb13491e
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 14:44:21 +0330

    Changed the button text to 'Hide' when the content is visible, and 'Show' when the content is hidden.

--- src/Components/ReactMecanism.jsx
+++ src/Components/ReactMecanism.jsx
@@ -2,15 +2,18 @@ import { useState } from "react"
 
 function ReactMecanism() {
     const [isShow, setIsShow] = useState(false);
-  return (
-    <div className="px-5">
-        <h1>React Mecanism</h1>
-        {isShow && <h3>session-118 </h3>}
-        {isShow && <button className="btn btn-success" onClick={()=>setIsShow((isShow)=>!isShow)}>hide</button>}
-        {!isShow && <button className="btn btn-info" onClick={()=>setIsShow((isShow)=>!isShow)}>show</button>}
-
+    return (
+        <div className="px-5">
+            <h1>React Mecanism</h1>
+            {isShow && <h3>session-118 </h3>}
+            <button
+                className={`btn ${isShow ? 'btn-success' : 'btn-info'}`}
+                onClick={() => setIsShow((isShow) => !isShow)}
+            >
+                {isShow ? 'Hide' : 'Show'}
+            </button>
         </div>
-  )
+    )
 }
 
 export default ReactMecanism
