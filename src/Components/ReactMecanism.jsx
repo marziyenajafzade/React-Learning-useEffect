ed4a3790954bb88377907326c1b26d46b2595ba6
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 14:37:57 +0330

    Add show/hide button to ReactMecanism component

--- src/Components/ReactMecanism.jsx
+++ src/Components/ReactMecanism.jsx
@@ -3,11 +3,12 @@ import { useState } from "react"
 function ReactMecanism() {
     const [isShow, setIsShow] = useState(false);
   return (
-    <div>
+    <div className="px-5">
         <h1>React Mecanism</h1>
         {isShow && <h3>session-118 </h3>}
-        
-        <button onClick={()=>setIsShow((isShow)=>!isShow)}>change</button>
+        {isShow && <button className="btn btn-success" onClick={()=>setIsShow((isShow)=>!isShow)}>hide</button>}
+        {!isShow && <button className="btn btn-info" onClick={()=>setIsShow((isShow)=>!isShow)}>show</button>}
+
         </div>
   )
 }
