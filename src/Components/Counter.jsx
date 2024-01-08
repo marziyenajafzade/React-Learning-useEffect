294c3d14cba59ac1893b5e434fd9f48bce4960b0
marziyenajafzadeh <marziyenajafzade@gmail.com>
2024-01-08 12:02:43 +0330

    Add a counter that increments every second.

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,14 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-const Counter = () =>{
-    useEffect(()=>{
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((count) => count + 1)
+        }, 1000);
 
         //unmouting state | claenup function
-        return ()=>{console.log('unmouting state')}
-    },[])
-  return (
-    <div>Counter</div>
-  )
+        return () => {
+            console.log('unmouting state');
+            clearInterval(interval);
+        }
+    }, [count])
+    return (
+        <div>Count: {count}</div>
+    )
 }
 
 export default Counter
